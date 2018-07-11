c84aebaf890cf5064cb6ea570e18a4ad5cbcb5d6
Travis Ludlum <tludlum@citrine.io>
2018-07-10 20:15:43 -0700

    Create component/mine_sweeper/ index file

---
+++ src/components/mine_sweeper/index.js
@@ -0,0 +1,7 @@
+import Board from './Board';
+import Header from './Header';
+import Tile from './Tile';
+import DifficultySelector from './DifficultySelector';
+import GameOverStatement from './GameOverStatement';
+
+export { Board, Header, Tile, DifficultySelector, GameOverStatement };
